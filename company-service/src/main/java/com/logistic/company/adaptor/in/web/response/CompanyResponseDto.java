ebf0d729850ee0f2cc55001aa5a9dfe993bfa7fc
seongmin <seongmin1117@gmail.com>
2025-03-13 17:16:39 +0900

    Feat: Company 기본 패키지 구조 생성

---
+++ company-service/src/main/java/com/logistic/company/adaptor/in/web/response/CompanyResponseDto.java
@@ -0,0 +1,14 @@
+package com.logistic.company.adaptor.in.web.response;
+
+import com.logistic.company.domain.CompanyType;
+import com.logistic.company.domain.vo.Address;
+
+public record CompanyResponseDto(
+    Long companyId,
+    String name,
+    CompanyType type,
+    Address address,
+    String manager,
+    Long hubId,
+    Boolean isDeleted) {
+}
