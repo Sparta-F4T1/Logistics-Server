bc016b0c18bc7ee717afa34402f553ce85382d55
kim0527 <joo14753@naver.com>
2025-03-15 21:29:59 +0900

    Chore: Delivery 필드명 수정

--- delivery-service/src/main/java/com/logistic/delivery/domain/Delivery.java
+++ delivery-service/src/main/java/com/logistic/delivery/domain/Delivery.java
@@ -9,25 +9,23 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Delivery {
-  private Long deliveryId;
+  private Long id;
   private Long orderId;
-  private DeliveryStatus deliveryStatus;
+  private DeliveryStatus status;
   private Long departHubId;
   private Long arrivalHubId;
   private String driverId;
 
   public static Delivery create(
-      Long deliveryId,
       Long orderId,
-      DeliveryStatus deliveryStatus,
+      DeliveryStatus status,
       Long departHubId,
       Long arrivalHubId,
       String driverId
   ){
     return Delivery.builder()
-        .departHubId(deliveryId)
         .orderId(orderId)
-        .deliveryStatus(deliveryStatus)
+        .status(status)
         .departHubId(departHubId)
         .arrivalHubId(arrivalHubId)
         .driverId(driverId)
