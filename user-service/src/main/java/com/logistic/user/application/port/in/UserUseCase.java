1cc14d03e343dd4e74255fcc91ca9910d4fe2225
KIM SOOBIN <xpsxm225@naver.com>
2025-03-21 11:58:10 +0900

    Feat: user-service project init
    Resolved: #12

--- user-service/src/main/java/com/logistic/user/application/port/in/UserUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.logistic.user.application.port.in;
-
-import com.logistic.user.application.port.in.command.TestCommand;
-
-public interface UserUseCase {
-  String createUser(TestCommand command);
-}
