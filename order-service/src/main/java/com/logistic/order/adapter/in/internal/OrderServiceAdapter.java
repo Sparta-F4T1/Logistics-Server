f6b61f6bd5f88a1f2240aefc4fe4c38e88af5460
kade <bhpark93@gmail.com>
2025-03-23 03:28:57 +0900

    Feat: 타 서비스에 Order 정보 제공 API 구현

---
+++ order-service/src/main/java/com/logistic/order/adapter/in/internal/OrderServiceAdapter.java
@@ -0,0 +1,27 @@
+package com.logistic.order.adapter.in.internal;
+
+import com.logistic.common.annotation.Adapter;
+import com.logistic.common.internal.response.OrderClientResponse;
+import com.logistic.order.adapter.in.internal.mapper.OrderInternalMapper;
+import com.logistic.order.application.port.in.OrderUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Adapter
+@RestController
+@RequestMapping("/internal/v1/orders")
+@RequiredArgsConstructor
+public class OrderServiceAdapter {
+
+  private final OrderUseCase orderUseCase;
+  private final OrderInternalMapper orderInternalMapper;
+
+  @GetMapping("/{orderId}")
+  public OrderClientResponse findOrder(@PathVariable("orderId") Long orderId){
+    return orderInternalMapper.toClientResponse(orderUseCase.findOrder(orderId));
+  }
+
+}
