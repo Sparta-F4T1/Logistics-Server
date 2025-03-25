c7fb764481e2ca8c921a152a34d4ee091f35d11f
seongmin <seongmin1117@gmail.com>
2025-03-25 09:58:24 +0900

    Fix: 공통 모듈 호환성을 위해 수정

--- company-service/src/main/java/com/logistic/company/adapter/in/internal/CompanyServiceAdapter.java
+++ company-service/src/main/java/com/logistic/company/adapter/in/internal/CompanyServiceAdapter.java
@@ -1,7 +1,6 @@
 package com.logistic.company.adapter.in.internal;
 
 import com.logistic.common.annotation.Adapter;
-import com.logistic.common.internal.request.CompanyClientRequest;
 import com.logistic.common.internal.response.CompanyClientResponse;
 import com.logistic.common.passport.annotation.WithPassport;
 import com.logistic.common.passport.model.Passport;
@@ -13,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,15 +26,15 @@ public class CompanyServiceAdapter {
 
   @GetMapping("/{companyId}")
   public CompanyClientResponse findCompany(@PathVariable("companyId") final Long companyId,
-                                           @WithPassport Passport passport,
-                                           @ModelAttribute final CompanyClientRequest request) {
+                                           @WithPassport Passport passport) {
     return mapper.toCompanyResponse(queryUseCase.findCompany(
-        mapper.toFindQuery(companyId, request)));
+        mapper.toFindQuery(companyId, passport)));
   }
 
   @GetMapping
-  public List<CompanyClientResponse> findCompanyList(@RequestBody final CompanyClientRequest request) {
-    return queryUseCase.findCompanyList(mapper.toListQuery(request))
+  public List<CompanyClientResponse> findCompanyList(@ModelAttribute final List<Long> companyIds,
+                                                     @WithPassport Passport passport) {
+    return queryUseCase.findCompanyList(mapper.toListQuery(companyIds, passport))
         .stream().map(mapper::toCompanyResponse).toList();
   }
 }
--- company-service/src/main/java/com/logistic/company/adapter/in/internal/mapper/CompanyServiceMapper.java
+++ company-service/src/main/java/com/logistic/company/adapter/in/internal/mapper/CompanyServiceMapper.java
@@ -1,20 +1,19 @@
 package com.logistic.company.adapter.in.internal.mapper;
 
-import com.logistic.common.internal.request.CompanyClientRequest;
 import com.logistic.common.internal.response.CompanyClientResponse;
+import com.logistic.common.passport.model.Passport;
 import com.logistic.company.application.port.in.query.FindCompanyQuery;
 import com.logistic.company.application.port.in.query.ListCompanyQuery;
 import com.logistic.company.domain.model.CompanyView;
+import java.util.List;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CompanyServiceMapper {
 
-  //  @Mapping(source = "request.passport", target = "passport")
-  FindCompanyQuery toFindQuery(Long companyId, CompanyClientRequest request);
+  FindCompanyQuery toFindQuery(Long companyId, Passport passport);
 
-  //  @Mapping(source = "request.passport", target = "passport")
-  ListCompanyQuery toListQuery(CompanyClientRequest request);
+  ListCompanyQuery toListQuery(List<Long> companyIds, Passport passport);
 
   CompanyClientResponse toCompanyResponse(CompanyView companyView);
 }
