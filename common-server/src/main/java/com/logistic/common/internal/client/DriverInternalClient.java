5bbc931716ee8b668e15259482455d6e16305ed9
seongmin <seongmin1117@gmail.com>
2025-03-26 15:58:38 +0900

    Fix: driver-service 코드 리팩토링 및 zipkin 수정

--- common-server/src/main/java/com/logistic/common/internal/client/DriverInternalClient.java
+++ common-server/src/main/java/com/logistic/common/internal/client/DriverInternalClient.java
@@ -5,13 +5,14 @@ import com.logistic.common.internal.response.DriverClientResponse;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface DriverInternalClient {
   @GetMapping("/internal/v1/drivers/{driverId}")
   DriverClientResponse findDriver(@PathVariable("driverId") String driverId);
 
-  @GetMapping("/internal/v1/drivers/hub")
-  List<DriverClientResponse> getHubDriver(@RequestParam DriverClientRequest request);
+  @PostMapping("/internal/v1/drivers/hub")
+  List<DriverClientResponse> getHubDriver(@RequestBody DriverClientRequest request);
 
 }
