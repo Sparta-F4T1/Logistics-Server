fe4a2024f981df6700035dcbe8baf3c8265a0b60
seongmin <seongmin1117@gmail.com>
2025-03-26 12:17:38 +0900

    Feat: Product Event 추가 및 수정

--- product-service/src/main/java/com/logistic/product/adapter/in/external/message/ProductEventAdapter.java
+++ product-service/src/main/java/com/logistic/product/adapter/in/external/message/ProductEventAdapter.java
@@ -1,22 +1,35 @@
 package com.logistic.product.adapter.in.external.message;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.logistic.common.annotation.Adapter;
 import com.logistic.common.internal.message.CancelOrderEvent;
 import com.logistic.common.internal.message.DeleteCompanyEvent;
 import com.logistic.product.adapter.in.external.message.mapper.ProductEventMapper;
 import com.logistic.product.application.port.in.ProductCommandUseCase;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
+@Slf4j(topic = "ProductEventAdapter")
 @Adapter
 @RequiredArgsConstructor
 public class ProductEventAdapter {
   private final ProductEventMapper mapper;
+  private final ObjectMapper objectMapper;
   private final ProductCommandUseCase commandUseCase;
 
   @RabbitListener(queues = "order.product")
-  public void cancelOrder(CancelOrderEvent cancelOrderEvent) {
-    commandUseCase.increaseStock(mapper.toUpdateStockCommand(cancelOrderEvent));
+  public void cancelOrder(String event) throws JsonProcessingException {
+    try {
+      CancelOrderEvent cancelOrderEvent = objectMapper.readValue(event, CancelOrderEvent.class);
+      commandUseCase.increaseStock(mapper.toUpdateStockCommand(cancelOrderEvent));
+    } catch (JsonProcessingException e) {
+      log.error(e.getMessage(), e);
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+    }
+
   }
 
   @RabbitListener(queues = "delete.company.product")
